969d6deaf613ebf22386271e1d3126dd41a45ab0
150630107045 <divyalpatel25@gmail.com>
2018-12-31 10:56:47 +0530

    First Commit

---
+++ fall_lib.py
@@ -0,0 +1,24 @@
+"""
+Fall Detection Algorithm
+"""
+def first_module(arg):    
+    """
+    Doc string for first module
+    """
+    print("module created successfuly")
+    return arg
+
+class First:
+    """
+    Doc string for first class module
+    """
+    def __init__(self):
+        
+        print("class created")
+
+    def first(self):
+        """
+        Doc string for class method
+        """
+        print("method created")
+        
